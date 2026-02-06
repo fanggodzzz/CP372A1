03be0bf799f87b8e8e0c8411232bf22b20a88567
Win Htein Oo <winhteinoo.26@gmail.com>
2026-02-05 17:18:32 -0800

    Add main method to ClientMain class

--- Client/ClientMain.java
+++ Client/ClientMain.java
@@ -1,1 +1,8 @@
-
+public class ClientMain {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            ClientGUI gui = new ClientGUI();
+            gui.setVisible(true);
+        });
+    }
+}
