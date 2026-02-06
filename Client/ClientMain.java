812998da131606043db2e923d234ef5b3d47f5c4
Win Htein Oo <winhteinoo.26@gmail.com>
2026-02-05 18:03:59 -0800

    Refactor GUI initialization in ClientMain

--- Client/ClientMain.java
+++ Client/ClientMain.java
@@ -1,8 +1,11 @@
+// ClientMain.java
+// The program starts
+
 public class ClientMain {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            ClientGUI gui = new ClientGUI();
-            gui.setVisible(true);
-        });
+        // Create the GUI window
+        ClientGUI gui = new ClientGUI();
+        // Show the window
+        gui.setVisible(true);
     }
 }
