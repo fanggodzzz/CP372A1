ea196f046848aaf3e18eff1473dc99ea284948b8
Win Htein Oo <winhteinoo.26@gmail.com>
2026-02-07 00:44:18 -0500

    Remove unused splitPane variable from ClientGUI

--- Client/ClientGUI.java
+++ Client/ClientGUI.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientGUI extends JFrame {
-    private JSplitPane splitPane;
+
     // Connection controls
     private JTextField ipField;
     private JTextField portField;
