d9b9aa420336c27cb9f90028a1bee9fdaf48d6fd
Win Htein Oo <winhteinoo.26@gmail.com>
2026-02-07 00:28:22 -0500

    Fix formatting in ClientMain.java

--- Client/ClientMain.java
+++ Client/ClientMain.java
@@ -4,7 +4,7 @@ public class ClientMain {
     public static void main(String[] args) {
         ClientGUI gui = new ClientGUI();
         gui.setVisible(true);
-        gui.fixSplit(); 
+        gui.fixSplit();
     }
 }
 
