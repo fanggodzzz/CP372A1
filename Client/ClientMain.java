7f1c47d8dadc7f1db04ab14c790ab1366bd9c50f
Win Htein Oo <winhteinoo.26@gmail.com>
2026-02-07 00:21:06 -0500

    Fix split method call in ClientMain

--- Client/ClientMain.java
+++ Client/ClientMain.java
@@ -1,10 +1,10 @@
 // ClientMain.java
-// The program starts
 
 public class ClientMain {
     public static void main(String[] args) {
         ClientGUI gui = new ClientGUI();
         gui.setVisible(true);
-        gui.fixSplit();
+        gui.fixSplit(); 
     }
 }
+
